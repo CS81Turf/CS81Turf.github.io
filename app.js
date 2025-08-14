7a079e4abde8b2cc2be61663947e78a9f195d744
ChrisH <chrishabich3@gmail.com>
2025-08-13 20:47:55 -0400

    More small CSS changes to light/dark mode

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ let isDark = false; // track state manually
 
 darkBtn.addEventListener('click', () => {
     if (!isDark) {
+        document.body.style.backgroundColor = '#39ff14';
         container.style.backgroundColor = 'black';
         container.style.color = '#c9d9e2ff'; 
         darkBtn.textContent = 'Light mode';
     } else {
+        document.body.style.backgroundColor = '#858fa1ff';
         container.style.backgroundColor = 'white';
         container.style.color = 'black';
         darkBtn.textContent = 'Dark mode';
